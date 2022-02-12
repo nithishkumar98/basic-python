b8e5da7936bc7bd456ea482060d8362f8126330d
Nithish kumar <nithishindians@gmail.com>
2022-02-12 19:43:29 +0530

    countdigit added

---
+++ countDigit.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the number to be counted??"))
+
+def count_digit(n):
+    count = 0
+    while n != 0:
+        n = n//10
+        count += 1
+    print(count)
+
+
+count_digit(n)
+
